df61f30f5521f46db5db05d09986b9cefacc6526
Debashis Badajena <deb.office01@gmail.com>
2023-10-27 16:00:01 +0530

    automated UI testing using puppeteer and JEST

--- tests/google_search_result.test.ts
+++ tests/google_search_result.test.ts
@@ -1,58 +1,48 @@
-const Puppeteer = require("puppeteer");
+import * as Puppeteer from "puppeteer";
 const url = "https://www.google.com/";
-test("checking the title", async () => {
-  const title: String = "Google";
 
-  const browser = await Puppeteer.launch({
-    args: ["--start-maximized"],
-    headless: false,
-    slowMo: 70,
+describe("Testing Google Search Bar", () => {
+  let browser: Puppeteer.Browser;
+  let page: Puppeteer.Page;
+
+  beforeAll(async () => {
+    browser = await Puppeteer.launch({
+      args: ["--start-maximized"],
+      headless: false,
+      slowMo: 70,
+    });
+    page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForTimeout(1000);
+    await page.setViewport({ width: 1360, height: 1080 });
+    await page.waitForTimeout(1000);
+  }, 30000);
+
+  afterAll(async () => {
+    await browser.close();
   });
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.setViewport({ width: 1360, height: 1080 });
-  //   await page.waitForTimeout(1000);
-  const pageTitle = await page.title();
-  expect(title).toBe(pageTitle);
-  await browser.close();
-}, 25000);
 
-test("should select the search element", async () => {
-  const browser = await Puppeteer.launch({
-    args: ["--start-maximized"],
-    headless: false,
-    slowMo: 70,
-  });
-  const searchSelector = 'textarea[title="Search"]';
+  test("Checking the title", async () => {
+    const title = "Google";
+    const pageTitle = await page.title();
+    expect(title).toBe(pageTitle);
+  }, 25000);
 
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.setViewport({ width: 1360, height: 1080 });
-  await page.waitForTimeout(2000);
-  expect(page.waitForSelector(searchSelector)).toBeTruthy();
-  await page.waitForTimeout(2000);
-  await browser.close();
-}, 25000);
+  test("Should select the search element", async () => {
+    const searchSelector = 'textarea[title="Search"]';
+    expect(page.waitForSelector(searchSelector)).toBeTruthy();
+  }, 25000);
 
-test("Google search and redirection", async () => {
-  const browser = await Puppeteer.launch({
-    args: ["--start-maximized"],
-    headless: false,
-    slowMo: 70,
-  });
-  const searchSelector = 'textarea[title="Search"]';
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.setViewport({ width: 1360, height: 1080 });
-  await page.waitForTimeout(2000);
-  expect(page.waitForSelector(searchSelector)).toBeTruthy();
-  await page.waitForTimeout(2000);
-  await page.click(searchSelector, { clickCount: 1 });
-  await page.type(searchSelector, "chimera technologies");
-  await page.keyboard.press("Enter");
-  await page.waitForTimeout(2000);
-  const redirectedUrl = await page.url();
-  expect(redirectedUrl).not.toBe(url);
-  await browser.close();
-}, 25000);
-
+  test("should search and redirection", async () => {
+    const searchSelector = 'textarea[title="Search"]';
+    await page.waitForTimeout(2000);
+    await page.waitForSelector(searchSelector);
+    await page.click(searchSelector, { clickCount: 1 });
+    await page.type(searchSelector, "chimera technologies");
+    await page.keyboard.press("Enter");
+    await page.waitForTimeout(2000);
+    const redirectedUrl = await page.url();
+    expect(redirectedUrl).not.toBe(url);
+    await browser.close();
+  }, 25000);
+});
